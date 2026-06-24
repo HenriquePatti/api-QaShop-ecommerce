fde20b21679594d727c9a630b8d3ac18eece7a61
Henrique Patti <rezendepatti@gmail.com>
2026-06-24 14:23:22 -0300

    test(auth): automatiza AUTH-01 CT-05 a CT-07 (prioridade média)

--- tests/auth/auth-01.test.js
+++ tests/auth/auth-01.test.js
@@ -49,4 +49,47 @@ describe('[AUTH-01] [POST /auth/register]: Campos obrigatórios no cadastro', ()
     expect(res.body.error.details.fieldErrors.email).to.deep.equal(['Required']);
     expect(res.body.error.details.fieldErrors.password).to.deep.equal(['Required']);
   })
+
+  it('CT-05 - cadastro com `name` vazio - 400 VALIDATION_ERROR', async () => {
+    const body = { name: '', email: 'ct05@test.com', password: 'Senha123' };
+    const res = await request(BASE_URL)
+      .post('/auth/register')
+      .send(body);
+    expect(res.status).to.equal(400);
+    expect(res.body).to.have.nested.property('error.code', 'VALIDATION_ERROR');
+    expect(res.body.error.details).to.have.property('fieldErrors');
+    expect(res.body.error.details.fieldErrors.email).to.be.undefined;
+    expect(res.body.error.details.fieldErrors.password).to.be.undefined;
+    expect(res.body.error.details.fieldErrors.name).to.deep.equal(['Name is required']);
+  })
+
+  it('CT-06 - cadastro com `email` vazio - 400 VALIDATION_ERROR', async () => {
+    const body = { name: 'Teste 06', email: '', password: 'Senha123' };
+    const res = await request(BASE_URL)
+      .post('/auth/register')
+      .send(body);
+    expect(res.status).to.equal(400);
+    expect(res.body).to.have.nested.property('error.code', 'VALIDATION_ERROR');
+    expect(res.body.error.details).to.have.property('fieldErrors');
+    expect(res.body.error.details.fieldErrors.name).to.be.undefined;
+    expect(res.body.error.details.fieldErrors.password).to.be.undefined;
+    expect(res.body.error.details.fieldErrors.email).to.deep.equal(['Invalid email']);
+  })
+
+  it('CT-07 - cadastro com `password` vazio - 400 VALIDATION_ERROR', async () => {
+    const body = { name: 'Teste 07', email: 'ct07@test.com', password: '' };
+    const res = await request(BASE_URL)
+      .post('/auth/register')
+      .send(body);
+    expect(res.status).to.equal(400);
+    expect(res.body).to.have.nested.property('error.code', 'VALIDATION_ERROR');
+    expect(res.body.error.details).to.have.property('fieldErrors');
+    expect(res.body.error.details.fieldErrors.name).to.be.undefined;
+    expect(res.body.error.details.fieldErrors.email).to.be.undefined;
+    expect(res.body.error.details.fieldErrors.password).to.deep.equal([
+      'Password must be at least 8 characters',
+      'Password must contain at least 1 uppercase letter',
+      'Password must contain at least 1 number']
+    );
+  })
 })
